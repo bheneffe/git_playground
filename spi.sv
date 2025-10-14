410d00849cd807346b78fb7586a0cb9ad7e97399
Bastien Heneffe <bheneffe@expected-it.com>
2025-10-14 15:41:27 +0200

    Add SPI iternface

---
+++ spi.sv
@@ -0,0 +1,10 @@
+module spi (
+    input logic clk,
+    input logic rst_n,
+    input logic mosi,
+    output logic miso,
+    input logic sck,
+    input logic cs_n
+);
+    // SPI implementation here
+endmodule
